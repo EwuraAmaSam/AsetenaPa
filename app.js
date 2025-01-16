d827f8325b6d0674b10c948d1a0a4d6d1faa1f27
Ewura Ama Etruwaa Sam <ewura.sam@ashesi.edu.gh>
2025-01-16 13:17:10 +0000

    Add firebase

--- app.js
+++
@@ -1,8 +0,0 @@
-
-// document.getElementById('toSignUp').addEventListener('click', function () {
-//     window.location.href = "signup.html";  
-// });
-
-// document.getElementById('toSignIn').addEventListener('click', function () {
-//     window.location.href = "signin.html";  
-// });
---
+++ public/firebase-config.js
@@ -0,0 +1,17 @@
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyD4yLgmzNRqNWwhOqFStE3zbhzwYguvBXA",
+    authDomain: "asetenapa-d0183.firebaseapp.com",
+    projectId: "asetenapa-d0183",
+    storageBucket: "asetenapa-d0183.firebasestorage.app",
+    messagingSenderId: "511219784343",
+    appId: "1:511219784343:web:2e6b11e8fe9812147ce7d2",
+    measurementId: "G-4LHMQM4GMM"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
