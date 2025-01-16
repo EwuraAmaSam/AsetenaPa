cdd3a03eb1c29f214b6d74f1efa5aa0f59c6fcad
Ewura Ama Etruwaa Sam <ewura.sam@ashesi.edu.gh>
2025-01-16 00:25:51 +0000

    Add welcome, signup, signin and temporary homepage

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// document.getElementById('toSignUp').addEventListener('click', function () {
+//     window.location.href = "signup.html";  
+// });
+
+// document.getElementById('toSignIn').addEventListener('click', function () {
+//     window.location.href = "signin.html";  
+// });
